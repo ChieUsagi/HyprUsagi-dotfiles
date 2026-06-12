d21b75d707fa00e378e6ffcc98c2fa499ecb0346
ChieUsagi <tsukiselenes@gmail.com>
2026-06-12 18:52:34 -0500

    Delete config/hypr/update directory

--- config/hypr/update/version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# version.sh: Display version information
-# https://github.com/Antiz96/arch-update
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-# shellcheck disable=SC2154
-echo "${name} ${version}"
